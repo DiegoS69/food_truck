c188fb0c94f30c404bf66c0983695378c07695ee
Diego Sotomayor <diego.i.sotomayor.s@gmail.com>
2026-09-03 22:16:44 -0400

    clase 02-09

--- pedidos/views.py
+++ pedidos/views.py
@@ -1,15 +1,11 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 from .models import Producto
 
 def catalogo(request):
     productos = Producto.objects.all()
-    texto = "Catalogo\n\n"
-    for p in productos:
-        texto += f"{p.nombre} - "
-        texto += f"{p.categoria.nombre} - "
+    return render(request,
+        "pedidos/catalogo.html",
+        {"productos": productos})
 
-    return HttpResponse(texto,
-        content_type="text/plain: charset=utf-8") 
 
 # Create your views here.
